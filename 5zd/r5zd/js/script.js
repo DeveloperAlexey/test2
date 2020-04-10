7c01b63f1fa933ab5209a1443dc988bacabca999
Developer <dollar123dollar@gmail.com>
2020-04-10 21:06:58 +0200

    10April

--- 5zd/r5zd/js/script.js
+++ 5zd/r5zd/js/script.js
@@ -1,9 +1,17 @@
 let menuItem = document.querySelector('.menu'),
     body = document.getElementsByTagName('body'),
-    lin = document.createElement('li');
+    lin = document.createElement('li'),
+    title = document.getElementById('title'),
+    adv = document.querySelector('.column .adv'),
+    column = document.querySelectorAll('.column'),
+    prom = document.getElementById('prompt');
 body[0].style.backgroundImage = ("url(img/apple_true.jpg)");//'/img/apple_true.jpg';
 lin.innerHTML = 'пятый пункт';
 lin.classList.add('menu-item');
 menuItem.appendChild(lin);
+column[1].removeChild(adv);
+let prom_ = prompt('attitude towards apple?');
+prom.innerHTML = prom_;
 
-console.log(body);
+title.innerHTML = 'Мы продаем только подлинную технику Apple';
+console.log(adv);
